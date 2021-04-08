8a9959f3a8b1825a19663ba1672dea4d5084c3f9
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-08 20:52:11 -0300

    fix: correcting code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'], // diretorio raiz do projeto
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // Quero cobertura de teste em todos os arquivos dentro de src e que sejam ts
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/**/protocols/**',
+    '!<rootDir>/**/test/**'
+  ], // Quero cobertura de teste em todos os arquivos dentro de src e que sejam ts
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
