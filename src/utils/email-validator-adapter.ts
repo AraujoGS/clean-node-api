7447355dba53aab5aa1cf763cbdb47c17b8429b3
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-08 21:42:10 -0300

    refactor: change file name

---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,15 @@
+import { EmailValidator } from '../presentation/protocols/email-validator'
+import validator from 'validator'
+
+/**
+ * Implementação do email validator, utilizando o padrão Adapter
+ * Nele independente de qual lib eu use para validar email, o meu código está desacoplado dela,
+ * caso queira mudar para outra ou implementar minha propria regra, basta alterar esse adapter para que
+ * a validação funcione.
+ */
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
