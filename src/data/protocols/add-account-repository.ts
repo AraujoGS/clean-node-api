4cea0dc3766972e293bcaa0f1f0e0dcc6c9cfcb1
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-09 20:37:44 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,11 @@
+/**
+ * Como o arquivo de imports dos protocolos para a implementação db-add-account é usada
+ * especificamente para a classe concreta, não vai ser utilizada aqui.
+ * Os imports serão direto do domain
+ */
+import { AccountModel } from '../../domain/models/account'
+import { AddAccountModel } from '../../domain/usecases/add-account'
+
+export interface AddAccountRepository {
+  add: (account: AddAccountModel) => Promise<AccountModel>
+}
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,4 +1,4 @@
-import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 
 /**
  * Implementação do protocolo AddAccount definido no domain, ou seja,
@@ -7,13 +7,16 @@ import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-a
  */
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return await new Promise(resolve => resolve(null))
   }
 }
