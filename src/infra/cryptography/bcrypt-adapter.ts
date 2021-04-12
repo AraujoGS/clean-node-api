9f8762e2ba7f197882df54249bf6b61752708fea
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-12 11:10:04 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encrypter'
+export class BcryptAdapter implements Encrypter {
+  /**
+   * O salt é uma caracteristica especifica da lib Bcrypt, outros encriptadores
+   * não necessáriamente vão possuir essa propriedade, por isso ela é uma dependência
+   * injetada no construtor da classe que implementa o Bcrypt e não na interface
+   * que ele assina
+  */
+  private readonly salt: number
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await bcrypt.hash(value, 12)
+    return await new Promise(resolve => resolve(''))
+  }
+}
