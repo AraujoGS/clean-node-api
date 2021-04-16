240cc611ba792ee09d8d31f21fba15826daa6ac0
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-16 16:17:14 -0300

    test: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content-Type Middleware', () => {
+  test('Deve retornar por default com json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/) // utilizo uma regex porque o content-type json pode ser passado de muitas formas, por esse motivo caso o header tenha a palavra json consideramos válido
+  })
+  test('Deve retornar forçando o retorno xml', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
