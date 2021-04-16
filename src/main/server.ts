2349a1c1fb08b6927bd2dcf7d7fdd19d757081b8
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-16 19:35:41 -0300

    fix: calls correct app in server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,7 +3,5 @@
  * faz a composição de tudo que foi criado, realiza as injeções de dependência
  * necessárias e sobe nosso servidor (API)
 */
-import express from 'express'
-const app = express()
-
+import app from './config/app'
 app.listen(5050, () => console.log('server running at http://localhost:5050'))
