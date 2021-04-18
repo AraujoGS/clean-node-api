7bd3a19286175fa561ac37c3feee56b290718153
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-18 11:44:28 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,5 +3,12 @@
  * faz a composição de tudo que foi criado, realiza as injeções de dependência
  * necessárias e sobe nosso servidor (API)
 */
-import app from './config/app'
-app.listen(5050, () => console.log('server running at http://localhost:5050'))
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+
+MongoHelper.connect(env.mongoUri)
+  .then(async () => {
+    // somente após conectar no mongodb dou listen no meu servidor.
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`server running at http://localhost:${env.port}`))
+  }).catch(console.error)
