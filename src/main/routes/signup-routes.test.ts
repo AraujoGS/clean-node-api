7d8534a1ff9ee8c3eaa4adeed2f0f3efff2af910
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-18 11:11:34 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    // A lib 'jest-mongodb' seta a URL do mongo em memória dentro do env
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    // antes de cada teste limpo a base de dados
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Deve retornar uma conta criada quando sucesso', async () => {
     await request(app)
       .post('/api/signup')
