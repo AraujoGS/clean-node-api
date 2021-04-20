d0f231153df6b4d2f474ad99a8eab20aa2a95046
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-19 21:17:35 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,20 @@
+/**
+ * Decorator é um design pattern que consiste em adicionar comportamentos a um objeto
+ * existente em tempo de execução.
+ * É envolver um objeto com outro do mesmo tipo (Liskov Principle) e acrescer comportamentos a ele
+ */
+
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.controller.handle(httpRequest)
+    return await new Promise(resolve => resolve(null))
+  }
+}
