4237713077cd075c69b4eef574cd7f7b69d9048e
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-24 11:38:03 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -74,55 +74,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SignUp Controller', () => {
-  test('deve retornar 400 se o name não for informado', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.body).toEqual(new MissingParamError('name'))
-  })
-  test('deve retornar 400 se o email não for informado', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.body).toEqual(new MissingParamError('email'))
-  })
-  test('deve retornar 400 se password não for informado', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.body).toEqual(new MissingParamError('password'))
-  })
-  test('deve retornar 400 se passwordConfirmation não for informado', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
-  })
   test('deve retornar 400 se as senhas informadas são diferentes', async () => {
     const { sut } = makeSut()
     const httpRequest = {
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Controller, EmailValidator, Validation } from './signup-protocols'
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import { badRequest, internalServerError, ok } from '../../helpers/http-helper'
 import { AddAccount } from '../../../domain/usecases/add-account'
 
@@ -19,13 +19,6 @@ export class SignUpController implements Controller {
       const clientError = this.validation.validate(httpRequest.body)
       if (clientError) {
         return badRequest(clientError)
-      }
-      const requiredFiels = ['name', 'email', 'password', 'passwordConfirmation']
-
-      for (const field of requiredFiels) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
       }
       const { name, email, password, passwordConfirmation } = httpRequest.body
 
