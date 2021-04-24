c2ac3191b4d3809ba355601c5a5d75d5036748ea
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-24 11:05:39 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,5 +1,6 @@
 // Esse arquivo contem os imports/exports de todas as interfaces utilizadas no SignUpController
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
+export * from '../../helpers/validators/validation'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, Validation } from './signup-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, internalServerError, ok } from '../../helpers/http-helper'
 import { AddAccount } from '../../../domain/usecases/add-account'
@@ -6,14 +6,17 @@ import { AddAccount } from '../../../domain/usecases/add-account'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+  private readonly validation: Validation
 
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const requiredFiels = ['name', 'email', 'password', 'passwordConfirmation']
 
       for (const field of requiredFiels) {
