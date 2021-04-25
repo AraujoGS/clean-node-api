8e06bceb5b02397c5f5112589acc8b7561b82b01
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-25 15:20:54 -0300

    refactor: move Validation to  protocols folder

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,5 +1,5 @@
 // Esse arquivo contem os imports/exports de todas as interfaces utilizadas no SignUpController
 export * from '../../protocols'
-export * from '../../helpers/validators/validation'
+export * from '../../protocols/validation'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
