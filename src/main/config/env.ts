43a8308c86dc0d158d29634982e0820feb824498
Guilherme AS <guilhermearaujo421@gmail.com>
2021-04-29 17:09:08 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUri: process.env.MONGO_URL || 'mongodb://suaconexaocomomongodb',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'seuSecret'
 }
