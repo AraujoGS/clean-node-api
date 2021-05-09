c1e227b76b4b21af940273ff48bb45de9e927d63
Guilherme AS <guilhermearaujo421@gmail.com>
2021-05-09 20:49:28 -0300

    test: ensure AddSurveyController returns 201 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, internalServerError } from '../../../helpers/http/http-helper'
+import { badRequest, created, internalServerError } from '../../../helpers/http/http-helper'
 
 const makeFakeHttpRequest = (): HttpRequest => ({
   body: {
@@ -73,5 +73,10 @@ describe('AddSurvey Controller', () => {
     jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeHttpRequest())
     expect(httpResponse).toEqual(internalServerError(new Error()))
+  })
+  test('deve retornar 201 se a enquete for criada com sucesso', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(created())
   })
 })
