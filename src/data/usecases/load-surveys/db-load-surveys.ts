4ff6cffce1b7eddb9fa548dffeb645a3451e014f
Guilherme AS <guilhermearaujo421@gmail.com>
2021-05-19 21:22:29 -0300

    feat: ensure DbLoadSurveys returns surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.ts
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -6,7 +6,10 @@ export class DbLoadSurveys implements LoadSurveys {
   constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
 
   async load (): Promise<SurveyModel[]> {
-    await this.loadSurveysRepository.loadAll()
+    const surveys = await this.loadSurveysRepository.loadAll()
+    if (surveys.length) {
+      return surveys
+    }
     return []
   }
 }
