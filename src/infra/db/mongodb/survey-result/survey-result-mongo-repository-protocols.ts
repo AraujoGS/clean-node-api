403ee58f50baeb9a038c56f20884cc4bdf3040a5
Guilherme AS <guilhermearaujo421@gmail.com>
2021-05-28 22:00:46 -0300

    refactor: organize project files

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
@@ -1,4 +1,4 @@
-export * from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
+export * from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 export * from '@/domain/models/survey'
 export * from '@/domain/models/account'
 export * from '@/infra/db/mongodb/helpers/mongo-helper'
