ae959bd91fe1e5a6efbe6ecda083b45a87f74b7f
Guilherme AS <guilhermearaujo421@gmail.com>
2021-06-16 19:28:50 -0300

    refactor: use builder patterns to build the aggregation query

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
@@ -1,4 +1,4 @@
 export * from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 export * from '@/domain/models/survey'
 export * from '@/domain/models/account'
-export * from '@/infra/db/mongodb/helpers/mongo-helper'
+export * from '@/infra/db/mongodb/helpers'
