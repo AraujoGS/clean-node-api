6791421a74873b5ecc319bc01f50eea50e1bea90
Guilherme AS <guilhermearaujo421@gmail.com>
2021-06-20 15:33:20 -0300

    test: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,17 @@
+import { DbLoadSurveyResult } from './db-load-survey-result'
+import { SurveyResultModel, mockSurveyResultModel, LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('deve chamar o LoadSurveyResultRepository', async () => {
+    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+      async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+        return await Promise.resolve(mockSurveyResultModel())
+      }
+    }
+    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
+    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_survey_id')
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})
