01d6f8f0bc554fa2bc9b96e3845a0b35ede87284
Guilherme AS <guilhermearaujo421@gmail.com>
2021-06-27 12:52:39 -0300

    docs: add load survey result path

--- src/main/docs/paths.ts
+++ src/main/docs/paths.ts
@@ -1,8 +1,8 @@
-import { loginPath, loadSurveysPath, signUpPath, addSurveyPath, saveSurveyResultPath } from './paths/'
+import { loginPath, loadSurveysPath, signUpPath, addSurveyPath, saveSurveyResultPath, loadSurveyResultPath } from './paths/'
 
 export default {
   '/login': loginPath,
   '/signup': signUpPath,
   '/surveys': { ...loadSurveysPath, ...addSurveyPath }, // como os dois endpoints possuem a mesma URL mesclei os arquivos separados
-  '/surveys/{surveyId}/results': saveSurveyResultPath
+  '/surveys/{surveyId}/results': { ...saveSurveyResultPath, ...loadSurveyResultPath } // como os dois endpoints possuem a mesma URL mesclei os arquivos separados
 }
