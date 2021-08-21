302fac2c1c01b9f073001c8f53c324966a252f58
Guilherme AS <guilhermearaujo421@gmail.com>
2021-08-21 15:40:10 -0300

    feat: create usecase CheckAccountByEmailRepository

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,6 +1,6 @@
 import { AddAccount } from '@/domain/usecases'
 import { Hasher } from '@/data/protocols/cryptography'
-import { AddAccountRepository, LoadAccountByEmailRepository } from '@/data/protocols/db'
+import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols/db'
 /**
  * Implementação do protocolo AddAccount definido no domain, ou seja,
  * implementação da minha regra de negocio. Nesse caso utilizando uma abordagem
@@ -10,13 +10,13 @@ export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
   ) {}
 
   async add (params: AddAccount.Params): Promise<AddAccount.Result> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(params.email)
+    const exists = await this.checkAccountByEmailRepository.checkByEmail(params.email)
     let isValid: boolean = false
-    if (!account) {
+    if (!exists) {
       const hashedPassword = await this.hasher.hash(params.password)
       isValid = await this.addAccountRepository.add(Object.assign({}, params, { password: hashedPassword }))
     }
